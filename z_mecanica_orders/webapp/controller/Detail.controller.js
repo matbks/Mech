19484f2ab7067ef086046aa1ef378bec0f333cf8
matbks <mateus.bks@hotmail.com>
2022-03-26 19:10:52 -0300

    orderItemsCrudOK

--- z_mecanica_orders/webapp/controller/Detail.controller.js
+++ z_mecanica_orders/webapp/controller/Detail.controller.js
@@ -2,8 +2,11 @@ sap.ui.define([
 	"./BaseController",
 	"sap/ui/model/json/JSONModel",
 	"../model/formatter",
-	"sap/m/library"
-], function (BaseController, JSONModel, formatter, mobileLibrary) {
+	"sap/m/library",
+	"sap/m/MessageBox",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+], function (BaseController, JSONModel, formatter, mobileLibrary, MessageBox, Filter, FilterOperator) {
 	"use strict";
 
 	// shortcut for sap.m.URLHelper
@@ -17,14 +20,19 @@ sap.ui.define([
 		/* lifecycle methods                                           */
 		/* =========================================================== */
 
-		onInit : function () {
+		onInit: function () {
 			// Model used to manipulate control states. The chosen values make sure,
 			// detail page is busy indication immediately so there is no break in
 			// between the busy indication for loading the view's meta data
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter
+				.getRoute("order")
+				.attachPatternMatched(this._onObjectMatched, this);
+
 			var oViewModel = new JSONModel({
-				busy : false,
-				delay : 0,
-				lineItemListTitle : this.getResourceBundle().getText("detailLineItemTableHeading")
+				busy: false,
+				delay: 0,
+				lineItemListTitle: this.getResourceBundle().getText("detailLineItemTableHeading")
 			});
 
 			this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
@@ -32,6 +40,18 @@ sap.ui.define([
 			this.setModel(oViewModel, "detailView");
 
 			this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
+
+			this._data = {
+				order_items: [],
+			};
+
+			this.jModel = new JSONModel(this._data);
+
+
+
+
+
+
 		},
 
 		/* =========================================================== */
@@ -42,23 +62,13 @@ sap.ui.define([
 		 * Event handler when the share by E-Mail button has been clicked
 		 * @public
 		 */
-		onSendEmailPress : function () {
-			var oViewModel = this.getModel("detailView");
-
-			URLHelper.triggerEmail(
-				null,
-				oViewModel.getProperty("/shareSendEmailSubject"),
-				oViewModel.getProperty("/shareSendEmailMessage")
-			);
-		},
-
 
 		/**
 		 * Updates the item count within the line item table's header
 		 * @param {object} oEvent an event containing the total number of items in the list
 		 * @private
 		 */
-		onListUpdateFinished : function (oEvent) {
+		onListUpdateFinished: function (oEvent) {
 			var sTitle,
 				iTotalItems = oEvent.getParameter("total"),
 				oViewModel = this.getModel("detailView");
@@ -85,21 +95,25 @@ sap.ui.define([
 		 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
 		 * @private
 		 */
-		 _onObjectMatched: function (oEvent) {
+		_onObjectMatched: function (oEvent) {
 			var sObjectId = oEvent.getParameter("arguments").objectId;
-			this.sCustomerCpf = sObjectId;
+			this.sOrderId = sObjectId;
 			this.getModel("device").setProperty("/layout", "TwoColumnsBeginExpanded");
+
 			this.getModel()
-			  .metadataLoaded()
-			  .then(
-				function () {
-				  var sObjectPath = this.getModel().createKey("CustomersSet", {
-					Cpf: sObjectId,
-				  });
-				  this._bindView("/" + sObjectPath);
-				}.bind(this)
-			  );
-		  },
+				.metadataLoaded()
+				.then(
+					function () {
+						var sObjectPath = this.getModel().createKey("OrdersSet", {
+							Id: sObjectId,
+						});
+						this._bindView("/" + sObjectPath);
+					}.bind(this)
+				);
+
+			this.getItems(this.sOrderId);
+
+		},
 
 		/**
 		 * Binds the view to the object path. Makes sure that detail view displays
@@ -108,7 +122,7 @@ sap.ui.define([
 		 * @param {string} sObjectPath path to the object to be bound to the view.
 		 * @private
 		 */
-		_bindView : function (sObjectPath) {
+		_bindView: function (sObjectPath) {
 			// Set busy indicator during view binding
 			var oViewModel = this.getModel("detailView");
 
@@ -116,10 +130,10 @@ sap.ui.define([
 			oViewModel.setProperty("/busy", false);
 
 			this.getView().bindElement({
-				path : sObjectPath,
+				path: sObjectPath,
 				events: {
-					change : this._onBindingChange.bind(this),
-					dataRequested : function () {
+					change: this._onBindingChange.bind(this),
+					dataRequested: function () {
 						oViewModel.setProperty("/busy", true);
 					},
 					dataReceived: function () {
@@ -129,7 +143,7 @@ sap.ui.define([
 			});
 		},
 
-		_onBindingChange : function () {
+		_onBindingChange: function () {
 			var oView = this.getView(),
 				oElementBinding = oView.getElementBinding();
 
@@ -157,19 +171,19 @@ sap.ui.define([
 				oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
 		},
 
-		_onMetadataLoaded : function () {
+		_onMetadataLoaded: function () {
 			// Store original busy indicator delay for the detail view
 			var iOriginalViewBusyDelay = this.getView().getBusyIndicatorDelay(),
 				oViewModel = this.getModel("detailView"),
-				oLineItemTable = this.byId("lineItemsList"),
-				iOriginalLineItemTableBusyDelay = oLineItemTable.getBusyIndicatorDelay();
+				oLineItemTable = this.byId("pes")
+			iOriginalLineItemTableBusyDelay = oLineItemTable.getBusyIndicatorDelay();
 
 			// Make sure busy indicator is displayed immediately when
 			// detail view is displayed for the first time
 			oViewModel.setProperty("/delay", 0);
 			oViewModel.setProperty("/lineItemTableDelay", 0);
 
-			oLineItemTable.attachEventOnce("updateFinished", function() {
+			oLineItemTable.attachEventOnce("updateFinished", function () {
 				// Restore original busy indicator delay for line item table
 				oViewModel.setProperty("/lineItemTableDelay", iOriginalLineItemTableBusyDelay);
 			});
@@ -202,11 +216,11 @@ sap.ui.define([
 				this.getModel("appView").setProperty("/layout", "MidColumnFullScreen");
 			} else {
 				// reset to previous layout
-				this.getModel("appView").setProperty("/layout",  this.getModel("appView").getProperty("/previousLayout"));
+				this.getModel("appView").setProperty("/layout", this.getModel("appView").getProperty("/previousLayout"));
 			}
 		},
 
-		onPress: function(oEvent) {
+		onPress: function (oEvent) {
 			var oItem = oEvent.getSource();
 			var sPath = oItem.getBindingContext().getPath("Plate");
 			var sPath1 = oItem.getBindingContext().getPath("Customerid");
@@ -214,13 +228,203 @@ sap.ui.define([
 			var modelData = oTable.getModel();
 			var data = modelData.getProperty(sPath);
 			var data1 = modelData.getProperty(sPath1);
-	  
+
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("order", {
-			  invoicePath: data,
-			  invoicePath1: data1
+				invoicePath: data,
+				invoicePath1: data1
 			});
-		  },
+		},
+
+		addRow: function (oArg) {
+			this._data.order_items.push({ Description: "", Quantity: "", Price: "", Type: "", Unit: "" });
+			this.jModel.refresh(); //which will add the new record
+		},
+
+		deleteRow: function (oEvent) {
+			var deleteRecord = oEvent
+				.getSource()
+				.getBindingContext("servicesAndProducts");
+			//	.getObject();
+			//	.getPath();
+			for (var i = 0; i < this._data.order_items.length; i++) {
+				if (this._data.order_items[i] == deleteRecord.getObject()) {
+
+					var line = this._data.order_items[i];
+
+					if (line.Description != '' && line.Id != '' && line.Item != '' && line.Quantity != '' && line.Type != '' && line.Price != '' && line.Unit != '') {
+						var oModel = this.getView().getModel();
+
+						oModel.remove("/OrderItemsSet(Id='" + deleteRecord.getObject().Id + "'," + "Item='" + deleteRecord.getObject().Item + "')", {
+							success: function (oData, oResponse) {
+								if (oResponse.statusCode == "204") {
+									var msg = this.getOwnerComponent().getModel("i18n").getResourceBundle().getText("deleted");
+									MessageBox.success(msg);
+
+								}
+							}.bind(this),
+							error: function (oError) {
+								var oSapMessage = JSON.parse(oError.responseText);
+								var msg = oSapMessage.error.message.value;
+								MessageBox.error(msg);
+							},
+						});
+					}
+					this._data.order_items.splice(i, 1); //removing 1 record from i th index.
+					this.jModel.refresh();
+				}
+			}
+		},
+
+		getItems: function (id) {
+
+			var oFilter = [
+				new Filter("Id", FilterOperator.EQ, id),
+			];
+			var oModel = this.getView().getModel();
+			oModel.read("/OrderItemsSet", {
+				filters: oFilter,
+				success: function (oData, oResponse) {
+					this._data.order_items = oData.results;
+					this.jModel.refresh();
+					this.byId("pes").getModel().refresh(true);
+				}.bind(this),
+				error: function (oError) {
+					this.jModel.setData(this._data);
+				}.bind(this),
+			});
+		},
+
+		// getItems: function(id) {
+
+		// 	if ( this.select("/OrderItemsSet", this._data.order_items, "Id", id) == "0" ) 
+		// 	{
+		// 		this.jModel.refresh();
+		// 		this.byId("pes").getModel().refresh(true);
+		// 	}
+		// 	else{ this.jModel.setData(this._data); }
+
+		// },
+
+		onBeforeRendering: function () {
+
+			this.byId("pes").setModel(this.jModel, "servicesAndProducts");
+		},
+		onNavBack: function () {
+			history.go(-2);
+		},
+
+		onClearMessages: function () {
+			this.getModel("orderView").setProperty(
+				"/messageIcon",
+				"sap-icon://message-success"
+			);
+
+			sap.ui.getCore().getMessageManager().removeAllMessages();
+
+		},
+
+		saveRow: function (oEvent) {
+
+			var createItemError = false
+			var createItemErrorLog;
+			var keys_to_be_deleted = [];
+
+			for (let key in this._data.order_items) {
+
+				if (this._data.order_items[key].Id == undefined && this._data.order_items[key].Item == undefined) {
+					var line = this._data.order_items[key];
+
+
+					if (line.Description != '' && line.Id != '' && line.Item != '' && line.Quantity != '' && line.Type != '' && line.Price != '' && line.Unit != '') {
+						var item = key;
+						item = ++item;
+
+						line.Id = this.sOrderId;
+						line.Item = item.toString();
+						line.Description = line.Description;
+						line.Quantity = line.Quantity.toString();
+						line.Type = line.Type;
+						line.Price = line.Price.toString();
+						line.Unit = line.Unit.toString();
+
+						var oModel = this.getView().getModel();
+
+						oModel.create("/OrderItemsSet", line, {
+							success: function (oData, oResponse) {
+								if (
+									key == this._data.order_items.length - 1 &&
+									createItemError == false
+								) {
+									this._clearOrderTable();
+								}
+							}.bind(this),
+							error: function (oError) {
+								createItemError = true;
+								createItemErrorLog = "Falha na criação do item da linha " + key;
+							}.bind(this),
+						});
+					}
+					else if (line.Description != '' || line.Quantity != '' || line.Type != '' || line.Price != '' || line.Unit != '')
+					{	
+						var errorLine = parseInt(key) + 1;
+						createItemError = true;
+						createItemErrorLog = "Preencha todos os dados da linha " + errorLine;
+					}
+					else {
+						keys_to_be_deleted.push(key);
+					}
+				}
+				else {
+					var updateItemError = false;
+					var updateItemErrorLog;
+					var line = this._data.order_items[key];
+
+					var item = key;
+					item = ++item;
+
+					line.Id = this.sOrderId;
+					line.Item = item.toString();
+					line.Description = line.Description;
+					line.Quantity = line.Quantity.toString();
+					line.Type = line.Type;
+					line.Price = line.Price.toString();
+					line.Unit = line.Unit.toString();
+
+					var oModel = this.getView().getModel();
+
+					oModel.update("/OrderItemsSet(Id='" + line.Id + "'," + "Item='" + line.Item + "')", line, 
+					{
+						method: "PUT",
+						success: function(oData, oResponse) { sap.m.MessageToast.show(" updated Successfully");}, 
+						error: function(oError) {sap.m.MessageToast.show("failure");} 
+					});
+				}
+			}
+
+			for (let x in keys_to_be_deleted.sort((a, b) => b - a)) 
+			{
+				this._data.order_items.splice(keys_to_be_deleted[x], 1); //removing 1 record from i th index.
+			}
+
+			this.jModel.refresh();
+
+			if (updateItemError && createItemError == false) 
+			{
+				MessageBox.success(updateItemErrorLog);
+			}
+			else if (updateItemError == false && createItemError)
+			{
+				MessageBox.success(createItemErrorLog);
+			}
+			else 
+			{ 
+				MessageBox.success("Salvo com sucesso");
+			}
+		}
+
+
+
 	});
 
 });
